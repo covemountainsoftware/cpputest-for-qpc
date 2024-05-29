3672c4bc590c6073fe129f5235653d119074fe2c
Matthew Eshleman <matthew@covemountainsoftware.com>
2024-05-29 18:46:11 -0500

    add CMS prefix to all internal cmake variables

--- cpputest-for-qpc-lib/cmake/qpcCMakeSupport.cmake
+++ cpputest-for-qpc-lib/cmake/qpcCMakeSupport.cmake
@@ -1,17 +1,17 @@
-set(QPC_SRC_DIR ${CMS_QPC_TOP_DIR}/src)
-set(QPC_INCLUDE_DIR ${CMS_QPC_TOP_DIR}/include)
-set(QPC_QF_SRC_DIR ${QPC_SRC_DIR}/qf)
+set(CMS_QPC_SRC_DIR ${CMS_QPC_TOP_DIR}/src)
+set(CMS_QPC_INCLUDE_DIR ${CMS_QPC_TOP_DIR}/include)
+set(CMS_QPC_QF_SRC_DIR ${CMS_QPC_SRC_DIR}/qf)
 
-set(QPC_QF_SRCS
-	${QPC_QF_SRC_DIR}/qep_hsm.c
-	${QPC_QF_SRC_DIR}/qep_msm.c
-	${QPC_QF_SRC_DIR}/qf_act.c
-	${QPC_QF_SRC_DIR}/qf_actq.c
-	${QPC_QF_SRC_DIR}/qf_defer.c
-	${QPC_QF_SRC_DIR}/qf_dyn.c
-	${QPC_QF_SRC_DIR}/qf_mem.c
-	${QPC_QF_SRC_DIR}/qf_ps.c
-	${QPC_QF_SRC_DIR}/qf_qact.c
-	${QPC_QF_SRC_DIR}/qf_qeq.c
-	${QPC_QF_SRC_DIR}/qf_qmact.c
-	${QPC_QF_SRC_DIR}/qf_time.c)
+set(CMS_QPC_QF_SRCS
+	${CMS_QPC_QF_SRC_DIR}/qep_hsm.c
+	${CMS_QPC_QF_SRC_DIR}/qep_msm.c
+	${CMS_QPC_QF_SRC_DIR}/qf_act.c
+	${CMS_QPC_QF_SRC_DIR}/qf_actq.c
+	${CMS_QPC_QF_SRC_DIR}/qf_defer.c
+	${CMS_QPC_QF_SRC_DIR}/qf_dyn.c
+	${CMS_QPC_QF_SRC_DIR}/qf_mem.c
+	${CMS_QPC_QF_SRC_DIR}/qf_ps.c
+	${CMS_QPC_QF_SRC_DIR}/qf_qact.c
+	${CMS_QPC_QF_SRC_DIR}/qf_qeq.c
+	${CMS_QPC_QF_SRC_DIR}/qf_qmact.c
+	${CMS_QPC_QF_SRC_DIR}/qf_time.c)
