aac03857fd72d7d6f21117eccedb3d08065a3ffa
Matthew Eshleman <matthew@covemountainsoftware.com>
2026-01-09 09:47:32 -0600

    add stddef.h, prep for QP/C 8.1.2

--- cpputest-for-qpc-lib/include/qep_port.h
+++ cpputest-for-qpc-lib/include/qep_port.h
@@ -8,7 +8,7 @@
 
 #include <stdint.h>  /* Exact-width types. WG14/N843 C99 Standard */
 #include <stdbool.h> /* Boolean type.      WG14/N843 C99 Standard */
-
+#include <stddef.h>  /* for size_t */
 #include "qp.h"     /* QEP platform-independent public interface */
 
 #endif   // CPPUTEST_FOR_QPC_QEP_PORT_H
